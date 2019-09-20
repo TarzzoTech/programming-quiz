e17a9f57007cbc66f798921131cbdb51789935e1
Vijay Deepak <vijaydeepak7195@outlook.com>
2019-09-20 07:34:03 +0530

    Added Quiz result  connections

--- src/controllers/quiz.controller.js
+++ src/controllers/quiz.controller.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const Quiz = require("../models/quiz.model");
 
+// get all quiz entries
 router.get("/all-entries", (req, res, next) => {
-    res.status(200).json({
-        result: []
-    });
+    Quiz.find().then(quizEntries => {
+        res.status(200).json(quizEntries);
+    }).catch(err => next(err));
 });
 
+// insert quiz result
 router.post("/quiz-entry", (req, res, next) => {
-    res.status(200).json(true);
+    Quiz.collection.insert(req.body).then(data => {
+        res.status(200).json(true);
+    }).catch(err => next(err));
 });
 
 module.exports = router;
